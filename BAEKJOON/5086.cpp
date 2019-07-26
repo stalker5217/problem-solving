acd91f2b4c99a988f66c9234e21e576c7a757be7
song <stalker5217@gmail.com>
2019-07-27 01:00:21 +0900

    배수와 약수

---
+++ BAEKJOON/5086.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a, b;
+
+    while(true){
+        cin >> a >> b;
+
+        if(a == 0 && b == 0){
+            break;
+        }
+
+        
+        if(b % a == 0){
+            cout << "factor" << "\n";
+        }
+
+        else if(a % b == 0){
+            cout << "multiple" << "\n";
+        }
+
+        else if((a % b != 0) && (b % a != 0)){
+            cout << "neither" << "\n";
+        }
+    }
+
+    return 0;
+}
