4a52bb581540cc621960d127440953992089d95c
song <stalker5217@gmail.com>
2019-08-05 23:03:34 +0900

    cpp base

---
+++ .vscode/base.cpp
@@ -0,0 +1,23 @@
+#define DEBUG 0
+#define LOG cout
+
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    /* Algorithm
+    
+    
+    
+     */
+    
+    return 0;
+}
