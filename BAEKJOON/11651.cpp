9a1d73f111d5cc33b0dcc1912f31560167f3897e
Song <stalker5217@gmail.com>
2019-10-21 23:38:44 +0900

    11651 : 좌표 정렬하기 2

---
+++ BAEKJOON/11651.cpp
@@ -0,0 +1,50 @@
+#define DEBUG 0
+#define LOG(string) cout << string
+
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+bool compare(pair<int, int> a, pair<int, int> b);
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // Algorithm
+    int N;
+	cin >> N;
+	vector<pair<int, int> > mVector;
+	for(int i = 0 ; i < N ; i++){
+		int a, b;
+		cin >> a >> b;
+		mVector.push_back(make_pair(a, b));
+	}
+
+	sort(mVector.begin(), mVector.end(), compare);
+
+	for(int i = 0 ; i < N ; i++){
+		cout << mVector[i].first << " " << mVector[i].second << "\n";
+	}
+    
+    return 0;
+}
+
+bool compare(pair<int, int> a, pair<int, int> b){
+	if(a.second < b.second)
+		return true;
+	else if(a.second > b.second)
+		return false;
+	else{
+		if(a.first < b.first){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+}
