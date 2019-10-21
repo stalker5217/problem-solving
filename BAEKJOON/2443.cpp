7e9edaa166bbc8c4502bc424e6e0ee5ff913b744
Song <stalker5217@gmail.com>
2019-10-22 00:07:10 +0900

    2443 : 별찍기 - 6

---
+++ BAEKJOON/2443.cpp
@@ -0,0 +1,31 @@
+#define DEBUG 0
+#define LOG(string) cout << string
+
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // Algorithm
+    int N;
+	cin >> N;
+	
+	for(int i = N ; i > 0 ; i--){
+		for(int a = 0 ; a < N-i ; a++)
+			cout << " ";
+		
+		for(int b = 0 ; b < 2 * i - 1 ; b++)
+			cout << "*";
+
+		cout << "\n";
+	}
+    
+    return 0;
+}
