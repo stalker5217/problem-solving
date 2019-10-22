25e4b5a585c0f18088f9f6860ac2c252eebb6f89
Song <stalker5217@gmail.com>
2019-10-22 21:42:41 +0900

    2743 : 단어 길이 재기

---
+++ BAEKJOON/2743.cpp
@@ -0,0 +1,21 @@
+#define DEBUG 0
+#define LOG(string) cout << string
+
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+	string temp;
+	cin >> temp;
+	cout << temp.length();
+   
+    return 0;
+}
