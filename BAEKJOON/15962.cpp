4bf5f2573d866e4269c3f80ae5374bf1edc8785e
Song <stalker5217@gmail.com>
2019-10-23 22:56:51 +0900

    15962 : 새로운 시작

---
+++ BAEKJOON/15962.cpp
@@ -0,0 +1,20 @@
+#define DEBUG 0
+#define LOG(string) cout << string
+
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // Algorithm
+    cout << "파이팅!!";
+    
+    return 0;
+}
