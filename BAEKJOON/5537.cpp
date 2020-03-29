dcdb84fb4a1b069d05ef1381fd5f922bf42f69ca
Song <stalker5217@gmail.com>
2020-03-29 23:23:51 +0900

    5537 : welcome

---
+++ BAEKJOON/5537.cpp
@@ -0,0 +1,21 @@
+#define DEBUG 0
+#define LOG(string) cout << string
+
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cout << ".  .   ." << "\n";
+    cout << "|  | _ | _. _ ._ _  _" << "\n";
+    cout << "|/\\|(/.|(_.(_)[ | )(/.";
+
+    return 0;
+}
