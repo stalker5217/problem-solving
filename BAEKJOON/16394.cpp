ab305021fae5aeb46da42728a1c71c377b451b2d
Song <stalker5217@gmail.com>
2020-04-08 23:42:51 +0900

    16394 : 홍익대학교

---
+++ BAEKJOON/16394.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int curYear;
+	cin >> curYear;
+
+	cout << curYear - 1946;
+
+    return 0;
+}
