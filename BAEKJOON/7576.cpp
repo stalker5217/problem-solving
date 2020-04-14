2a2c484cb0f7dff6ded10472dc980943bed68300
Song <stalker5217@gmail.com>
2020-04-14 23:11:33 +0900

    7576 : 토마토

--- BAEKJOON/7576.cpp
+++ BAEKJOON/7576.cpp
@@ -6,14 +6,11 @@ using namespace std;
 
 int rowSize, colSize;
 int box[1000][1000];
-int preBox[1000][1000];
 
-bool checkPossible();
 void checkTomato();
 
 queue<pair<int, int>> welldone;
-int remainCnt = 0;
-int day = 0;
+int day = -1;
 
 int main(){
     ios_base::sync_with_stdio(false);
@@ -33,79 +30,60 @@ int main(){
             if(box[i][j] == 1){
                 welldone.push(make_pair(i, j));
             }
-            else if(box[i][j] == 0){
-                remainCnt++;
-            }
         }
     }
 
-    if(!checkPossible()){
-         cout << "-1"; 
-    }
-    else{
-       while(remainCnt > 0){
-           checkTomato();
-       }
+    checkTomato();
 
-       cout << day;
+    bool isPossible = true;
+    for(int i = 0 ; i < rowSize ; i++){
+        for(int j = 0 ; j < colSize ; j++){
+            // cout << box[i][j] << " ";
+            if(box[i][j] == 0){
+                isPossible = false;
+                break;
+            }
+        }
+        if(!isPossible) break;
+        // cout << "\n";
     }
 
+    if(isPossible) {cout << day;}
+    else {cout << "-1";}
 
     return 0;
 }
 
-bool checkPossible(){
-    bool possibleAll = true;
-    for(int i = 0 ; i < rowSize ; i++){
-        for(int j = 0 ; j < colSize ; j++){
-            if(box[i][j] == 0){
-                bool possible = false;
-                if(i-1 >= 0 && (box[i-1][j] == 0 || box[i-1][j] == 1)) possible = true;
-                else if(i+1 < rowSize && (box[i+1][j] == 0 || box[i+1][j] == 1)) possible = true;
-                else if(j-1 >= 0 && (box[i][j-1] == 0 || box[i][j-1] == 1)) possible = true;
-                else if(j+1 < colSize && (box[i][j+1] == 0 || box[i][j+1] == 1)) possible = true;
-
-                if(possible == false){
-                    possibleAll = false;
-                    break;
-                }
-            }
-        }
-    }
-
-    return possibleAll;
-}
-
 void checkTomato(){
-    day++;
+    while(true){
+        int daySize = welldone.size(); // 이 날 돌릴 사이즈
 
-    int daySize = welldone.size(); // 이 날 돌릴 사이즈
-    for(int a = 0 ; a < daySize ; a++){
-        pair<int, int> curPos = welldone.front();
-        welldone.pop();
+        if(daySize > 0) day++;
+        else break;
+        
+        for(int a = 0 ; a < daySize ; a++){
+            pair<int, int> curPos = welldone.front();
+            welldone.pop();
 
-        int i = curPos.first;
-        int j = curPos.second;
+            int i = curPos.first;
+            int j = curPos.second;
 
-        if(i-1 >= 0 && (box[i-1][j] == 0)){
-            box[i-1][j] = 1;
-            remainCnt--;
-            welldone.push(make_pair(i-1, j));
-        }
-        if(i+1 < rowSize && (box[i+1][j] == 0)){
-            box[i+1][j] = 1;
-            remainCnt--;
-            welldone.push(make_pair(i+1, j));
-        }
-        if(j-1 >= 0 && (box[i][j-1] == 0)){
-            box[i][j-1] = 1;
-            remainCnt--;
-            welldone.push(make_pair(i, j-1));
-        }
-        if(j+1 < colSize && (box[i][j+1] == 0)){
-            box[i][j+1] = 1;
-            remainCnt--;
-            welldone.push(make_pair(i, j+1));
+            if(i-1 >= 0 && (box[i-1][j] == 0)){
+                box[i-1][j] = 1;
+                welldone.push(make_pair(i-1, j));
+            }
+            if(i+1 < rowSize && (box[i+1][j] == 0)){
+                box[i+1][j] = 1;
+                welldone.push(make_pair(i+1, j));
+            }
+            if(j-1 >= 0 && (box[i][j-1] == 0)){
+                box[i][j-1] = 1;
+                welldone.push(make_pair(i, j-1));
+            }
+            if(j+1 < colSize && (box[i][j+1] == 0)){
+                box[i][j+1] = 1;
+                welldone.push(make_pair(i, j+1));
+            }
         }
     }
 }
