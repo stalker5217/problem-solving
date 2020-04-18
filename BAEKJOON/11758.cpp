758e0f3afa38ad1f7477346240fed8f5631a4a7b
Song <stalker5217@gmail.com>
2020-04-18 17:36:33 +0900

    11758 : ccw

--- BAEKJOON/11758.cpp
+++ BAEKJOON/11758.cpp
@@ -30,15 +30,6 @@ int main(){
     return 0;
 }
 
-
 int ccw(int x1, int y1, int x2, int y2, int x3, int y3) {
-    int temp = x1*y2+x2*y3+x3*y1;
-    temp = temp - y1*x2-y2*x3-y3*x1;
-    if (temp > 0) {
-        return 1;
-    } else if (temp < 0) {
-        return -1;
-    } else {
-        return 0;
-    }
+    return (x2- x1) * (y3 - y1) - (y2- y1) * (x3 - x1);
 } 
