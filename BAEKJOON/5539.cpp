1c18cafc098d950a33d1a4b46bfe89ed776ee568
Song <stalker5217@gmail.com>
2020-04-19 00:17:34 +0900

    5539 : 콜센터

---
+++ BAEKJOON/5539.cpp
@@ -0,0 +1,28 @@
+#define DEBUG 0
+#define LOG(string) cout << string
+
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+	cout << "     /~\\" << endl;
+	cout << "    ( oo|" << endl;
+	cout << "    _\\=/_" << endl;
+	cout << "   /  _  \\" << endl;
+	cout << "  //|/.\\|\\\\" << endl;
+	cout << " ||  \\ /  ||" << endl;
+	cout << "============" << endl;
+	cout << "|          |" << endl;
+	cout << "|          |" << endl;
+	cout << "|          |" << endl;
+    
+    return 0;
+}
