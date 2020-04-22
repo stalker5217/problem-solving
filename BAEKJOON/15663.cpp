dcd7e76a1db59b34a6d38683a701b08cdc48f62f
Song <stalker5217@gmail.com>
2020-04-22 21:56:21 +0900

    15663 : N과 M (9)

---
+++ BAEKJOON/15663.cpp
@@ -0,0 +1,68 @@
+#define DEBUG 0
+#define LOG(string) cout << string
+
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <unordered_map>
+
+using namespace std;
+
+void backTracking();
+
+vector<int> sequence;
+vector<int> source;
+vector<bool> isPicked;
+unordered_map<string, int> isPrinted;
+
+int maxLength;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int numCnt;
+	cin >> numCnt >> maxLength;
+
+	for(int i = 0 ; i < numCnt ; i++){
+		int temp;
+		cin >> temp;
+		source.push_back(temp);
+	}
+	sort(source.begin(), source.end());
+    isPicked.resize(numCnt);
+    isPicked.assign(numCnt, false);
+
+	backTracking();
+
+    return 0;
+}
+
+void backTracking(){
+	if(sequence.size() == maxLength){
+		string key;
+        for(int i : sequence){
+			key += to_string(i);
+            key += ".";
+		}
+        
+        if(isPrinted[key] != 1){
+            for(int i : sequence) cout << i << " ";
+            cout << "\n";
+            isPrinted[key] = 1;
+        }
+		
+		return;
+	}
+	
+	for(int i = 0 ; i < source.size() ; i++){
+		if(isPicked[i] == false){
+            sequence.push_back(source[i]); isPicked[i] = true;
+            backTracking();
+            sequence.pop_back(); isPicked[i] = false;
+        }
+	}
+}
