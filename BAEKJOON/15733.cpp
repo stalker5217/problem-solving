8409a7a9c18bcebcef531e0adf554aa38e0c2d47
Song <stalker5217@gmail.com>
2020-04-25 18:57:47 +0900

    15733 : 나는 누구인가

---
+++ BAEKJOON/15733.cpp
@@ -0,0 +1,16 @@
+#define DEBUG 0
+#define LOG(string) cout << string
+
+#include <iostream>
+
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cout << "I'm Sexy";
+    
+    return 0;
+}
