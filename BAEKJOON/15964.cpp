99979c334905203c4384aea43db9abd54106c601
Song <stalker5217@gmail.com>
2020-05-21 21:56:06 +0900

    15964 : 이상한 기호

---
+++ BAEKJOON/15964.cpp
@@ -0,0 +1,18 @@
+#define DEBUG 0
+#define LOG(string) cout << string
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    long long a, b;
+	cin >> a >> b;
+
+	cout << (a+b)*(a-b);
+    
+    return 0;
+}
