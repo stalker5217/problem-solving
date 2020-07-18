6e7a154a37f5ae0e34503ec5c8b5dd51151fd079
Song <stalker5217@gmail.com>
2020-07-18 21:29:34 +0900

    9653 : 스타워즈 로고

---
+++ BAEKJOON/9653.cpp
@@ -0,0 +1,25 @@
+#define DEBUG 0
+#define LOG(string) cout << string
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cout << "    8888888888  888    88888" << "\n";
+    cout << "   88     88   88 88   88  88" << "\n";
+    cout << "    8888  88  88   88  88888" << "\n";
+    cout << "       88 88 888888888 88   88" << "\n";
+    cout << "88888888  88 88     88 88    888888" << "\n";
+    cout << "\n";
+    cout << "88  88  88   888    88888    888888" << "\n";
+    cout << "88  88  88  88 88   88  88  88" << "\n";
+    cout << "88 8888 88 88   88  88888    8888" << "\n";
+    cout << " 888  888 888888888 88  88      88" << "\n";
+    cout << "  88  88  88     88 88   88888888" << "\n";
+
+    return 0;
+}
