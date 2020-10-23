31cd3910e761f44069e4cf7a8d2c34576068ceba
Song <stalker5217@gmail.com>
2020-10-23 22:08:59 +0900

    12015 : 가장 긴 증가하는 부분 수열 2

---
+++ BAEKJOON/12015.cpp
@@ -0,0 +1,36 @@
+#define DEBUG 0
+#define LOG(string) cout << string
+#define INF 987654321
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	vector<int> lis;
+	lis.push_back(0);
+
+	for(int i = 0 ; i < n ; i++){
+		int cur;
+		cin >> cur;
+
+		if(cur > lis.back()) lis.push_back(cur);
+		else{
+			int idx = lower_bound(lis.begin(), lis.end(), cur) - lis.begin();
+			lis[idx] = cur;
+		}
+	}
+
+	int answer= lis.size() - 1;
+	cout << answer;
+	
+    return 0;
+}
