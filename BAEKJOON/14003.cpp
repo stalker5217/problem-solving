d78697f8faa6c2cbc1279d9e64335a868d8e017c
Song <stalker5217@gmail.com>
2020-11-04 23:34:06 +0900

    14003 : 가장 긴 증가하는 부분 수열 5

---
+++ BAEKJOON/14003.cpp
@@ -0,0 +1,55 @@
+#define DEBUG 0
+#define LOG(string) cout << string
+#define INF 987654321
+
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	int cnt = 0;
+	vector<int> lis;
+	vector<pair<int, int> > lisIdxValPair;
+	
+	lis.push_back(-1000000000);
+	for(int i = 0 ; i < n ; i++){
+		int cur;
+		cin >> cur;
+
+		if(cur > lis.back()){
+			lis.push_back(cur);
+			lisIdxValPair.push_back({++cnt, cur});
+		}
+		else{
+			int idx = lower_bound(lis.begin(), lis.end(), cur) - lis.begin();
+			lis[idx] = cur;
+			lisIdxValPair.push_back({idx, cur});
+		}
+	}
+
+	stack<int> valStack;
+	int idx = cnt;
+	for(int i = lisIdxValPair.size() - 1 ; i >= 0 ; i--){
+		if(lisIdxValPair[i].first == idx){
+			valStack.push(lisIdxValPair[i].second);
+			idx--;
+		}
+	}
+
+	cout << cnt << "\n";
+	while(!valStack.empty()){
+		cout << valStack.top() << " ";
+		valStack.pop();
+	}
+
+    return 0;
+}
