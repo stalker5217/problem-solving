8ad7aeca5eab920f7501e64c9ab756c0969c6f51
Song <stalker5217@gmail.com>
2020-12-01 21:23:12 +0900

    1365 : 꼬인 전깃줄

---
+++ BAEKJOON/1365.cpp
@@ -0,0 +1,42 @@
+#define DEBUG 0
+#define LOG(string) cout << string
+#define INF 987654321
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	vector<pair<int, int> > electricWire;
+	for(int i = 1 ; i <= n ; i++){
+		int connect;
+		cin >> connect;
+		electricWire.push_back({i, connect});
+	}
+	
+	vector<int> lis;
+	lis.push_back(-1000000000);
+
+	for(int i = 0 ; i < n ; i++){
+		int cur = electricWire[i].second;
+
+		if(cur > lis.back()) lis.push_back(cur);
+		else{
+			int idx = lower_bound(lis.begin(), lis.end(), cur) - lis.begin();
+			lis[idx] = cur;
+		}
+	}
+
+	int answer= n - lis.size() + 1;
+	cout << answer;
+	
+    return 0;
+}
