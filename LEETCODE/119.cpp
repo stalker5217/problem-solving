47fb89afe3295a738752f55381ba2eac753d0085
Song <stalker5217@gmail.com>
2021-04-01 13:28:50 +0900

    119 : Pascal's Triangle II

---
+++ LEETCODE/119.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+		int numRows = rowIndex + 1;
+        vector<vector<int> > pascalTriangle(numRows, vector<int>(0));
+
+		for(int row = 0 ; row < numRows ; row++){
+			pascalTriangle[row] = vector<int>(row + 1, 0);
+
+			for(int col = 0 ; col <= row ; col++){
+				if(col == 0 || col == row) pascalTriangle[row][col] = 1;
+				else pascalTriangle[row][col] = pascalTriangle[row-1][col-1] + pascalTriangle[row-1][col];
+			}
+		}
+        
+        return pascalTriangle[rowIndex];
+    }
+};
