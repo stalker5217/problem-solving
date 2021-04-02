53a656d4e0349ff7bb96e7bb21309879885b4864
Song <stalker5217@gmail.com>
2021-04-02 23:29:37 +0900

    112 : Path Sum

---
+++ LEETCODE/112.cpp
@@ -0,0 +1,22 @@
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    bool hasPathSum(TreeNode* node, int targetSum) {
+        if(node == nullptr) return false;
+        if(node->left == nullptr && node->right == nullptr && targetSum == node->val) return true;
+        return 
+            hasPathSum(node->left, targetSum - node->val) 
+            || 
+            hasPathSum(node->right, targetSum - node->val);
+    }
+};
