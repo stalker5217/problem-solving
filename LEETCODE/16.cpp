cbe3c6f6be7ae337c98c4ec628f76c48a07cfbf6
Song <stalker5217@gmail.com>
2021-04-10 20:38:51 +0900

    16 : 3Sum Closest

---
+++ LEETCODE/16.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        int ret = 987654321;
+        int closestDiff = 987654321;
+        
+        sort(nums.begin(), nums.end());
+        for(int i = 0 ; i < nums.size() - 2 ; i++){    
+            int start = i+1;
+            int end = nums.size() - 1;
+            
+            while(start < end){
+                int sum = nums[i] + nums[start] + nums[end];
+                
+                int curDiff = sum - target;
+                
+                if(abs(curDiff) < closestDiff){
+                    closestDiff = abs(curDiff);
+                    ret = sum;
+                }
+                
+                if(curDiff <= 0) start++;
+                else end--;
+            }
+        }
+        
+        return ret;
+    }
+};
