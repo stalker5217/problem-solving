c3cabddd0b6e880505469cc8f099189add3358c7
Song <stalker5217@gmail.com>
2021-04-15 01:57:49 +0900

    191 : Number of 1 Bits

---
+++ LEETCODE/191.cpp
@@ -0,0 +1,14 @@
+#include <cstdint>
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int ret = 0;
+        while(n > 0){
+            if(n & 1) ret++;
+            n >>= 1;
+        }
+        
+        return ret;
+    }
+};
