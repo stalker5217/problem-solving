f6ed06e8af3a460900fc1bf9d5ea769be36d7f33
Song <stalker5217@gmail.com>
2021-06-06 23:31:45 +0900

    94 : Binary Tree Inorder Traversal

---
+++ LEETCODE/94.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> result;
+        helper(root, result);
+        
+        return result;
+    }
+    
+    void helper(TreeNode* node, vector<int> & result) {
+        if(node) {
+            helper(node->left, result);
+            result.push_back(node->val);
+            helper(node->right, result);
+        }
+    }
+};
