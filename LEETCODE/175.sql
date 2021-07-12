8b7738648dc2838a6080e0414ea9867c3f1581e1
Song <stalker5217@gmail.com>
2021-07-12 23:48:44 +0900

    175 : Combine Two Tables

---
+++ LEETCODE/175.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+SELECT
+    P.FirstName,
+    P.LastName,
+    A.City,
+    A.State
+FROM
+    Person P
+LEFT OUTER JOIN
+    Address A
+ON
+    P.PersonId = A.PersonId;
