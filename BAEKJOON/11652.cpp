7d40e1d1616d2d34d9dbf1ff9d2ba8d390912e8d
Song <stalker5217@gmail.com>
2021-11-02 22:58:17 +0900

    11652 : 카드

--- BAEKJOON/11652.cpp
+++ BAEKJOON/11652.cpp
@@ -1,16 +1,44 @@
-#define _CRT_SECURE_NO_WARNINGS
-
 #include <iostream>
-#include <string>
+#include <vector>
+#include <algorithm>
+#include <climits>
 
 using namespace std;
 
-
-int main(int argc, char** argv)
+int main()
 {
-	char c;
-	cin >> c;
-	int a = c;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
 
-	cout << a << endl;
+	int n; cin >> n;
+	vector<long long> arr(n);
+	for(int i = 0 ; i < n ; i++) cin >> arr[i];
+
+	sort(arr.begin(), arr.end());
+
+	int maxCnt = -1;
+	long long maxCntValue = -1;
+	
+	long long pre = arr[0];
+	int cnt = 1;
+	for(int i = 1 ; i < arr.size() ; i++) {
+		if(arr[i] == pre) cnt++;
+		else {
+			if(maxCnt < cnt) {
+				maxCnt = cnt;
+				maxCntValue = pre;
+			}
+			cnt = 1;
+			pre = arr[i];
+		}
+	}
+
+	if(maxCnt < cnt) {
+		maxCnt = cnt;
+		maxCntValue = pre;
+	}
+
+	cout << maxCntValue;
+
+	return 0;
 }
