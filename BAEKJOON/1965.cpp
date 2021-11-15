3401ed3ff3191a2cae76f87df94c5ec597936aee
Song <stalker5217@gmail.com>
2021-11-15 19:59:21 +0900

    1965 : 상자넣기

---
+++ BAEKJOON/1965.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	int val[1000];
+	for(int i = 0 ; i < n ; i++){
+		cin >> val[i];
+	}
+
+	int dp[1000];
+	for(int i = 0 ; i < n ; i++){
+		dp[i] = 1;
+		for(int j = 0 ; j < i ; j++){
+			if(val[j] < val[i] && dp[j] >= dp[i]){
+				dp[i] = dp[j] + 1;
+			}
+		}
+	}
+
+	int answer = 0;
+	for(int i = 0 ; i < n ; i++){
+		answer = max(answer, dp[i]);
+	}
+
+	cout << answer;
+
+    return 0;
+}
