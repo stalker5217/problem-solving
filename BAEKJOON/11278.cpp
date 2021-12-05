0d6beb6268a11c2341e3ce9de1346b32ae4c00c4
Song <stalker5217@gmail.com>
2021-12-05 22:56:22 +0900

    11278 : 2-SAT - 2

---
+++ BAEKJOON/11278.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool isTrue(int val, int n) {
+	if(n > 0 && ((val >> (n - 1)) & 1)) return true;
+	else if(n < 0 && (((val >> (-n - 1)) & 1) == 0)) return true;
+
+	return false;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+	int n, m;
+	cin >> n >> m;
+
+	vector<pair<int, int> > clauses;
+	for(int i = 0 ; i < m ; i++) {
+		int a, b;
+		cin >> a >> b;
+		clauses.push_back({a, b});
+	}
+
+	bool possible;
+	int answer;
+	for(int i = 0 ; i < (1 << n) ; i++) {
+		possible = true;
+		
+		for(auto clause : clauses) {
+			if(!isTrue(i, clause.first) && !isTrue(i, clause.second)) {
+				possible = false;
+				break;
+			}
+		}
+
+		if(possible) {
+			answer = i;
+			break;
+		}
+	}
+
+	if(possible) {
+		cout << "1\n";
+		for(int i = 0 ; i < n ; i++, answer >>= 1) {
+			if(answer & 1) cout << "1 ";
+			else cout << "0 ";
+		}
+	}
+	else cout << 0; 
+
+    return 0;
+}
