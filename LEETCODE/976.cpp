521ce27b15cfce9676ad878490eb97fb8ba4ab57
songmk <stalker5217@gmail.com>
2022-04-26 09:19:10 +0900

    976 : Largest Perimeter Triangle

---
+++ LEETCODE/976.cpp
@@ -0,0 +1,19 @@
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int largestPerimeter(vector<int>& nums) {
+        sort(nums.begin(), nums.end(), greater<>());
+        
+        for(int i = 0 ; i < nums.size() - 2 ; i++) {
+            if (nums[i] < nums[i + 1] + nums[i + 2]) {
+                return nums[i] + nums[i + 1] + nums[i + 2];
+            }
+        }
+        
+        return 0;
+    }
+};
