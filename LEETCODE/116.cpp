52fdb30dbcf6c39e1bb1d2705cafcfe733cb3781
songmk <stalker5217@gmail.com>
2022-04-29 22:31:56 +0900

    116 : Populating Next Right Pointers in Each Node

--- LEETCODE/116.cpp
+++ LEETCODE/116.cpp
@@ -10,9 +10,9 @@ public:
     Node* right;
     Node* next;
 
-    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+    Node() : val(0), left(nullptr), right(nullptr), next(nullptr) {}
 
-    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+    Node(int _val) : val(_val), left(nullptr), right(nullptr), next(nullptr) {}
 
     Node(int _val, Node* _left, Node* _right, Node* _next)
         : val(_val), left(_left), right(_right), next(_next) {}
