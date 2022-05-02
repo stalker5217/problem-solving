731eec302ae31d64ff62d28ad4503721e8310618
songmk <stalker5217@gmail.com>
2022-05-02 09:34:34 +0900

    709 : To Lower Case

---
+++ LEETCODE/709.cpp
@@ -0,0 +1,14 @@
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string toLowerCase(string s) {
+        for(int i = 0 ; i < s.size() ; i++) {
+            s[i] = tolower(s[i]);
+        }
+        
+        return s;
+    }
+};
