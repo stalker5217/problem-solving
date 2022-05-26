8fc7262e130b217ff4f9882d90b5bae30969e029
songmk <stalker5217@gmail.com>
2022-05-26 23:40:03 +0900

    1502 : Can Make Arithmetic Progression From Sequence

---
+++ LEETCODE/1502.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        int diff = arr[1] - arr[0];
+        for(int i = 1 ; i < arr.size() - 1 ; i++) {
+            if(arr[i + 1] - arr[i] != diff) return false;
+        }
+        
+        return true;
+    }
+};
