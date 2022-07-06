6f23ab34477dfe2d90b3dd0ad3afc95ef5d8d43f
songmk <stalker5217@gmail.com>
2022-07-06 22:06:01 +0900

    559 : Maximum Depth of N-ary Tree

---
+++ LEETCODE/559.cpp
@@ -0,0 +1,39 @@
+#include <vector>
+
+using namespace std;
+
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+class Solution {
+public:
+    int maxDepth(Node* root) {
+        if (root == nullptr) {
+            return 0;
+        } else if (root->children.empty()) {
+            return 1;
+        }
+        
+        int answer = 0;
+        for(auto child : root->children) {
+            answer = max(answer, 1 + maxDepth(child));
+        }
+        
+        return answer;
+    }
+};
