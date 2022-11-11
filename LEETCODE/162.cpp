cb3fc2052a18071c6913298ef929e13d95c2d21a
songmk <stalker5217@gmail.com>
2022-11-11 23:36:11 +0900

    162 : Find Peak Element

---
+++ LEETCODE/162.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int start = 0;
+        int end = nums.size() - 1;
+        
+        while(start < end) {
+            int mid = (start + end) / 2;
+            if (nums[mid] < nums[mid + 1]) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+        
+        return start;
+    }
+};
