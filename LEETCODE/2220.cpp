3efb6d7015830a6e44abc264eef3e67394711a30
songmk <stalker5217@gmail.com>
2022-11-20 23:48:08 +0900

    2220 : Minimum Bit Flips to Convert Number

---
+++ LEETCODE/2220.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        int x = start ^ goal;
+        int cnt = 0;
+        while (x > 0) {
+            if (x & 1) {
+                cnt++;
+            }
+            
+            x >>= 1;
+        }
+        
+        return cnt;
+    }
+};
