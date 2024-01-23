9165b9bbefe8ca20f0facf4254c77169a672d57c
songmk <stalker5217@gmail.com>
2024-01-24 02:19:40 +0900

    577 : Employee Bonus

---
+++ LEETCODE/577.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+select
+    e.name,
+    b.bonus
+from
+    Employee e
+left outer join
+    Bonus b
+on
+    e.empId = b.empId
+where
+    b.bonus is null
+    OR b.bonus < 1000
